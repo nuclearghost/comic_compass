2377419fb71b73808c129fb96bb40e20fc8a2ca0
Prelang Builder <builder@prelang.com>
2014-07-20 18:50:22 +0000

    Defined validation for attribute 'phone' on model 'Shop'

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,5 @@ class Shop < ActiveRecord::Base
   belongs_to :user
   validates_formatting_of :zip, using: :us_zip
   validates_formatting_of :email, using: :email
+  validates_formatting_of :phone, using: :us_phone
 end
